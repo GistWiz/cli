ccc4a9abc0012cfa1e0e5155f5ba7b92cb659d1b
Wil (₩) Moore III <51953+wilmoore@users.noreply.github.com>
2024-12-04 22:40:44 -0700

    fix CORS and shorten provision script name to provision

--- src/cmd/serve.ts
+++ src/cmd/serve.ts
@@ -15,10 +15,12 @@ const corsPreflightRequestHandler = (req: any, res: any): boolean => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  if (req.method !== 'OPTIONS') false
-  res.writeHead(204)
-  res.end()
-  return true
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204)
+    res.end()
+    return true
+  }
+  return false
 }
 
 export async function serve(port: number) {
